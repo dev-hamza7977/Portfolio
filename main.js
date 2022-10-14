2a55e500bac3899c0072e44fa7d1f929efea8e57
dev-hamza7977 <85806118+dev-hamza7977@users.noreply.github.com>
2022-10-15 03:16:14 +0530

    updated with animations footer contact form and media query

---
+++ main.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    anime({
+        targets: '.port-text',
+        scale: [
+            { value: .1, easing: 'easeOutSine', duration: 500 },
+            { value: 1, easing: 'easeInOutQuad', duration: 1200 }
+        ],
+        delay: anime.stagger(200, { grid: [14, 5], from: 'center' })
+    });
+
+    anime({
+        targets: '.navbar',
+        translateY: [-50, 0],
+        easing: 'easeOutExpo',
+        duration: 1200,
+        delay: 1000,
+        opacity: [0, 1]
+    })
+
+    anime({
+        targets: '.hey-name',
+        translateX: 0,
+        scale: 1,
+        rotate: '1turn'
+    });
+
+    // anime({
+    //     targets: '.card',
+    //     scale: [
+    //         { value: .1, easing: 'easeOutSine', duration: 500 },
+    //         { value: 1, easing: 'easeInOutQuad', duration: 1200 }
+    //     ],
+    //     delay: anime.stagger(200, { grid: [14, 5], from: 'center' })
+    // });
+
+    anime({
+        targets: '.card',
+        translateX: [-50, 0],
+        easing: 'easeOutExpo',
+        duration: 1200,
+        delay: 1000,
+        opacity: [0, 1]
+    })
+
+    anime({
+        targets: '.contact-sec',
+        scale: [
+            { value: .1, easing: 'easeOutSine', duration: 500 },
+            { value: 1, easing: 'easeInOutQuad', duration: 1200 }
+        ],
+        delay: anime.stagger(200, { grid: [14, 5], from: 'center' })
+    });
+
+})
+
