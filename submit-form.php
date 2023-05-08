cba518afe903a5695f4a5f4248ff3642f445114c
dev-hamza7977 <85806118+dev-hamza7977@users.noreply.github.com>
2023-05-08 21:18:04 +0530

    Add files via upload

--- submit-form.php
+++ submit-form.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     fclose($file);
 
     // Redirect the user to a thank-you page
-    header("Location: thankyou.html");
+    // header("Location: thankyou.html");
     exit;
 }
 ?>
